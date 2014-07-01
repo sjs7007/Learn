6f3682549af96e0139885267316db2c02282561b
sjs7007 <sjs7007@gmail.com>
2014-07-01 19:38:15 +0530

    SPOJ LISA : All done. Next convert to spoj format and long long int for size issues.

--- Algos/DynamicProgramming/LISA.cpp
+++ Algos/DynamicProgramming/LISA.cpp
@@ -1,11 +1,15 @@
 //http://www.spoj.com/problems/LISA/
-// 18:48 - 19:26 max done,memoize it
+// 18:48 - 19:26 max done,memoize it - 19:31 memoized done,work for min function next
+//19:36 - All done for min! Submit.
 
 #include<iostream>
 using namespace std;
 #include<string.h> //forstrcpy
 
 int getOptimalMax(char inputArray[],int i,int j);
+int getOptimalMin(char inputArray[],int i,int j);
+int DPMax[100][100];
+int DPMin[100][100];
 
 int main()
 {
@@ -13,7 +17,12 @@ int main()
 	cin >>input;
 	char inputArray[input.length()+1];
 	strcpy(inputArray,input.c_str());
-	cout<<getOptimalMax(inputArray,0,input.length()-1);
+
+	memset(DPMax,-1,sizeof(DPMax));
+	memset(DPMax,-1,sizeof(DPMin));
+
+	//cout<<getOptimalMax(inputArray,0,input.length()-1);
+	cout<<getOptimalMax(inputArray,0,input.length()-1)<<" "<<getOptimalMin(inputArray,0,input.length()-1);
 }
 
 int getOptimalMax(char inputArray[],int i,int j)
@@ -23,10 +32,14 @@ int getOptimalMax(char inputArray[],int i,int j)
 		int temp = inputArray[i]-'0';
 		return temp;
 	}
+	else if(DPMax[i][j]>0)
+	{
+		//cout<<"Free Call!"<<endl;
+		return DPMax[i][j];
+	}
 	else
 	{
 		int max = -1;
-		int min = -1;
 
 		for(int k=i+1;k<=j-1;k=k+2)
 		{	
@@ -52,6 +65,52 @@ int getOptimalMax(char inputArray[],int i,int j)
 			}
 			//cout<<"test";
 		}
+		DPMax[i][j]=max;
 		return max;
+	}
+}
+
+int getOptimalMin(char inputArray[],int i,int j)
+{
+	if(i==j)
+	{
+		int temp = inputArray[i]-'0';
+		return temp;
+	}
+	else if(DPMin[i][j]>0)
+	{
+		//cout<<"Free Call!"<<endl;
+		return DPMin[i][j];
+	}
+	else
+	{
+		int min = -1;
+
+		for(int k=i+1;k<=j-1;k=k+2)
+		{	
+			
+		//	cout<<"Calling "<<i<<" "<<(k-1)<<" and "<<(k+1)<<" "<<j<<endl;
+			int temp1 = getOptimalMin(inputArray,i,k-1);
+			int temp2 = getOptimalMin(inputArray,k+1,j);
+			int temp3=-1;
+			
+		//	int temp1=2,temp2=4,temp3;
+			if(inputArray[k]=='+')
+			{
+				temp3 = temp1+temp2;
+			}
+			else if(inputArray[k]=='*')
+			{
+				temp3 = temp1*temp2;
+			}
+
+			if(temp3<min ||min==-1)
+			{
+				min=temp3;
+			}
+			//cout<<"test";
+		}
+		DPMin[i][j]=min;
+		return min;
 	}
 }
