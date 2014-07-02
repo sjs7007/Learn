f40f4abfd20b5e36f6b466fea6e783fe3103d1ff
sjs7007 <sjs7007@gmail.com>
2014-07-02 20:40:35 +0530

    STL stacks added.

---
+++ DataStructures/STLStacks.cpp
@@ -0,0 +1,19 @@
+//http://www.dreamincode.net/forums/topic/57497-stl-stack/
+
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+	stack <int> numbers;
+	numbers.push(1);
+	numbers.push(2);
+	numbers.push(3);
+	cout<<"Stack top : "<<numbers.top()<<endl;
+	//cout<<"Popped element : "<<numbers.pop()<<endl; NOTE : pop doesn't return anything so this causes errors.
+	numbers.pop(); 
+	cout<<"New stack top : "<<numbers.top()<<endl;
+	cout<<"Size of stack : "<<numbers.size()<<endl;
+}
