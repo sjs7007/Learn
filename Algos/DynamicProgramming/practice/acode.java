d4b5da986c79ae64bbb667338bc1efde865fa191
Saravanan SJ <sjs7007@gmail.com>
2015-09-29 19:57:37 -0400

    fixed error

--- Algos/DynamicProgramming/practice/acode.java
+++ Algos/DynamicProgramming/practice/acode.java
@@ -28,24 +28,26 @@ class acode
     public static long DP(int x[])
     {
         //int mul=1;
+        if(x.length==0)
+            return 1;
         long count=0;
 
-        if(x.length>=1)
+        if(x.length>=1 && x[0]>=1)
         {
             int y[]=new int[x.length-1];
             for(int i=1;i<x.length;i++)
                 y[i-1]=x[i];
             count = count + DP(y);
         }
-        if(x.length>=2 && (x[0]==1 || x[0]==2) && (x[1]>=1) && (x[1]<=6))
+        if(x.length>=2 && (x[0]==1 || x[0]==2) && (x[1]>=0) && (x[1]<=6))
         {
             int z[]=new int[x.length-2];
             for(int i=2;i<x.length;i++)
                 z[i-2]=x[i];
             count = count + DP(z);
         }
-        if(count==0)
-            count=1;
+       // if(count==0)
+         //   count=1;
         return count;
     }
 }
