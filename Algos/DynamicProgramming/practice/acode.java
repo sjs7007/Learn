d66a71f66436c2950ef1de71882b28642f649f49
Saravanan SJ <sjs7007@gmail.com>
2015-09-29 20:25:52 -0400

    memoized

--- Algos/DynamicProgramming/practice/acode.java
+++ Algos/DynamicProgramming/practice/acode.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 class acode
 {
+    static long DP[]=new long[6000];
+
     public static void main(String args[])
     {
         //int a[]={2,5,1,1,4};
@@ -11,9 +13,15 @@ class acode
         //int a[]={3,3,3,3,3,3,3,3,3,3};
         
         Scanner ip = new Scanner(System.in);
-        
+
+                
         while(true)
         {
+            
+            for(int i=0;i<DP.length;i++)
+            DP[i]=-1;
+
+            
             String input = ip.next();
             if(input.equals("0"))
                 break;
@@ -21,13 +29,15 @@ class acode
             for(int i=0;i<a.length;i++)
             a[i]=Character.getNumericValue(input.charAt(i));
             int n = a.length;
-            System.out.println(DP(a));
+            System.out.println(DP(a,0));
         }
     }
 
-    public static long DP(int x[])
+    public static long DP(int x[],int start)
     {
         //int mul=1;
+        if(DP[start]>0)
+            return DP[start];
         if(x.length==0)
             return 1;
         long count=0;
@@ -37,17 +47,18 @@ class acode
             int y[]=new int[x.length-1];
             for(int i=1;i<x.length;i++)
                 y[i-1]=x[i];
-            count = count + DP(y);
+            count = count + DP(y,start+1);
         }
         if(x.length>=2 && ((x[0]==1 && x[1]<=9) || (x[0]==2 && x[1]<=6)))
         {
             int z[]=new int[x.length-2];
             for(int i=2;i<x.length;i++)
                 z[i-2]=x[i];
-            count = count + DP(z);
+            count = count + DP(z,start+2);
         }
        // if(count==0)
          //   count=1;
+        DP[start]=count;
         return count;
     }
 }
