d0f78075ec5dfe6added52747b7d9802541bed05
Saravanan SJ <sjs7007@gmail.com>
2015-10-13 15:20:08 -0400

    goog

--- pracUF/Rotate2.java
+++ pracUF/Rotate2.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Rotate2 {
 	public void rotate(ArrayList<ArrayList<Integer>> a) {
 	    int n = a.get(0).size();
 	    while(n>1) {
@@ -7,13 +7,18 @@ public class Solution {
 	    }
 	    //return a;
 	}
+
+	public static void main(String )
 	
 	public ArrayList<ArrayList<Integer>>  rotateOnce(ArrayList<ArrayList<Integer>> x) {
-	   int r=0,c=0,n=x.get(0).size(),temp=x.get(0).get(0);
+	   int r=0,c=0,n=x.get(0).size(),temp;
 	   //go right
 	   //if(r==0)
 	   int rightMax = n-1,bottomMax=n-1,topMin=0,leftMin=0;
-	   while(leftMin<bottomMax) {
+	   int i=0;
+	   while(leftMin<rightMax) {
+	        temp = x.get(i).get(i);
+	        i++;
 		   	  while(c<rightMax) {
 		     //  temp=x.get(r).get(c);
 		       c++;
@@ -45,7 +50,7 @@ public class Solution {
 		   }
 		   //c=0 here
 		   //go up
-		   while(r>=topMin-1) {
+		   while(r>=topMin+1) {
 		     //  temp=x.get(r).get(c);
 		       r--;
 		       //swap x[r][c] and temp
@@ -53,11 +58,14 @@ public class Solution {
 		       x.get(r).set(c,temp);
 		       temp=sTemp;
 	 	  }
-	 	  rightMax++;
+	 	  //r = topMin-1
+	 	  r=i;
+	 	  c=i;
+	 	  rightMax--;
 	 	//  topMax++;
 	 	  bottomMax--;
-	 	  topMin--;
-	 	  leftMin--;
+	 	  topMin++;
+	 	  leftMin++;
 	   }
 	   return x;
 	}
