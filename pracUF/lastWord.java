31f78aaec2d855275c2153537f9edb2a6455af8f
Saravanan SJ <sjs7007@gmail.com>
2015-10-14 15:24:23 -0400

    ib powetr

---
+++ pracUF/lastWord.java
@@ -0,0 +1,53 @@
+public class lastWord {
+	public static int lengthOfLastWord(final String a) {
+	     int count=0;
+	    boolean flip=false;
+	    if(a.length()==0) {
+	        return 0;
+	    }
+	    else {
+	        for(int i=0;i<a.length();i++) {
+	           //loop until space
+	           for(int j=i;j<a.length();j++)
+	           {
+	               System.out.println("here1");
+	               if(a.charAt(j)==' ') {
+	                   j++;
+	               } 
+	               else
+	               {
+	                   count=0;
+	                   i=j;
+	                   break;
+	               }
+	               i=j;
+	           }
+	           //loop till next space 
+	           for(int j=i;j<a.length();j++) {
+	           		               System.out.println("here2");
+
+	               if(a.charAt(j)!=' ') {
+	                   count++;
+	               } else {
+	               	//	System.out.println(i);
+	               		i=j-1;
+	               		break;
+	               }
+	               //else {
+	              //     i=j-1;
+	               //    break;
+	              // }
+	               i=j;
+	           }
+
+
+	        }
+	    }
+	    return count;
+	}
+
+	public static void main(String args[])
+	{
+		System.out.println(lengthOfLastWord("Word1    "));
+	}
+}
--- pracUF/test.java
+++ pracUF/test.java
@@ -1,8 +1,10 @@
+import java.util.*;
+
 class test
 {
 	public static void main(String args[])
 	{
-		int x[][]={{1,2,3},{4,5,6},{7,8,9}};
+		/*int x[][]={{1,2,3},{4,5,6},{7,8,9}};
 		int n=3;
 		for(int c=0;c<n;c++)
 		{
@@ -22,6 +24,54 @@ class test
 			}
 			k++;
 			System.out.println();
-		}
+		}*/
+		findPrimeFactorsArray(10);
 	}
+
+	   public static ArrayList<Integer> findPrimeFactors(int a) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        int temp=a;
+        while(temp%2==0) {
+            ret.add(2);
+            temp=temp/2;
+        }
+        for(int i=3;i<=Math.sqrt(temp);i=i+2) {
+            while(temp%i==0) {
+                ret.add(i);
+                temp=temp/i;
+            }
+        }
+        if(temp>2) {
+            ret.add(temp);
+        }
+        System.out.println(ret);
+        return ret;
+    }
+
+     public static int[] findPrimeFactorsArray(int a) {
+        int ret[] = new int[a+1];
+        int temp=a;
+        while(temp%2==0) {
+            //ret.add(2);
+            ret[2]=ret[2]+1;
+            temp=temp/2;
+        }
+        for(int i=3;i<=Math.sqrt(temp);i=i+2) {
+            while(temp%i==0) {
+               // ret.add(i);
+                ret[i]=ret[i]+1;
+                temp=temp/i;
+            }
+        }
+        if(temp>2) {
+            //ret.add(temp);
+            ret[temp]=ret[temp]+1;
+        }
+       // System.out.println(ret);
+        for(int i=0;i<ret.length;i++) {
+        	if(ret[i]!=0)
+        		System.out.println(i+"^"+ret[i]);
+        }
+        return ret;
+    }
 }
